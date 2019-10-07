8de9ff0c671bf0e43689d1da428a03d0258c8e9d
Tanel Tekko <taneldeviis@hotmail.com>
2019-10-07 12:54:06 +0300

    front end service for news

---
+++ client/src/newsService.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+const url = "http://localhost:3000/api/news";
+
+class Service {
+    static getNews() {
+        return new Promise(async (resolve, reject) => {
+            try {
+                const res = await axios.get(url);
+                const data = res.data;
+                resolve(
+                    data.map(genre => ({
+                        ...genre
+                    }))
+                );
+            } catch (err) {
+                reject(err);
+            }
+        });
+    }
+}
+
+export default Service;
---
+++ client/src/views/News.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h1>News</h1>
+    </div>
+</template>
+
+<script>
+import newsService from '@/newsService'
+export default {
+    name: 'News',
+    data(){
+        return{
+            news: [],
+            feedback: ''
+        }
+    },
+    async created(){
+        try{
+            this.news = await newsService.getNews()
+        }
+        catch(err){
+            this.feedback = err
+        }
+    }
+}
+</script>
