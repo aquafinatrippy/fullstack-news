efe78d8873c79afa212e613e805c20d6fc5b1522
Tanel Tekko <tanel.tekko>
2019-10-07 13:45:45 +0300

    sessions and cookie parser installed

--- server/app.js
+++ server/app.js
@@ -2,19 +2,30 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const users = require("./routes/api/users");
-const news = require('./routes/api/news')
+const news = require("./routes/api/news");
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
 
+//middleware
 const app = express();
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 app.use(cors());
-
+app.use(
+  session({
+    secret: "secret",
+    saveUninitialized: true,
+    resave: true
+  })
+);
+//routes
 app.use("/api", users);
-app.use('/api', news)
+app.use("/api", news);
 
+//run server
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-    console.log(`server running on port ${port}`);
+  console.log(`server running on port ${port}`);
 });
--- server/routes/api/news.js
+++ server/routes/api/news.js
@@ -1,13 +1,53 @@
-const News = require('../../models/News')
-const router = require('express').Router()
+const News = require("../../models/News");
+const router = require("express").Router();
 
-router.get('/news/allNews', async (req, res) => {
-    try {
-        const news = News.find({})
-        res.send(news)
-    } catch (error) {
-        res.status(400).send(`Couldnt get all news: ${error}`)
-    }
-})
+router.get("/news/allNews", async (req, res) => {
+  try {
+    const news = News.find({});
+    res.send(news);
+  } catch (error) {
+    res.status(400).send(`Couldnt get all news: ${error}`);
+  }
+});
 
-module.exports = router
+router.post("/news", async (req, res) => {
+  try {
+    const news = new News({
+      author: req.body.author,
+      authorId: req.user._id,
+      title: req.body.title,
+      content: req.body.content
+    });
+    await news.save();
+    res.send(news);
+  } catch (error) {
+    res.status(400).send(`Couldnt post to news: ${error}`);
+  }
+});
+
+router.delete("/news/:id", async (req, res) => {
+  try {
+    const news = await News.findByIdAndDelete(req.params.id);
+    res.send(news);
+  } catch (error) {
+    res.status(400).send(`Failed to find and delete: ${error}`);
+  }
+});
+
+router.put("/news/:id", async (req, res) => {
+  try {
+    const news = await News.findByIdAndUpdate(
+      req.params.id,
+      {
+        title: req.body.title,
+        content: req.body.content
+      },
+      { new: true }
+    );
+    res.send(news);
+  } catch (error) {
+    res.status(400).send(`error updating: ${error}`);
+  }
+});
+
+module.exports = router;
