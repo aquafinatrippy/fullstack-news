9b00a08a8254a2a94230c9a0d11162477ed99b4a
Tanel Tekko <tanel.tekko>
2019-10-09 13:47:56 +0300

    axios fetch for frontend

--- client/src/main.js
+++ client/src/main.js
@@ -1,10 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import { TokenService } from "./services/storage.service";
+import { ApiService } from "./services/api.service";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+ApiService.init(process.env.VUE_APP_ROOT_API);
+if (TokenService.getToken()) {
+  ApiService.setHeader();
+}
 
 new Vue({
   router,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
--- client/src/registerService.js
+++
@@ -1,7 +0,0 @@
-const axios = require('axios')
-
-const url = "http://localhost:3000/api/register/"
-
-class RegisterService {
-    static registerUser()
-}
